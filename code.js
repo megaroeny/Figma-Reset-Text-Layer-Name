d76614c62705bf020d8f59780fa250acf31caade
Reony <reonytonneyck@gmail.com>
2019-10-09 23:26:28 -0400

    Added notification messaging
    Now the plugin provides notifications to the user for how many text layers were renamed, and if the selection is not a text layer.

--- code.js
+++ code.js
@@ -1,7 +1,12 @@
+let layerCount = (figma.currentPage.selection.length);
 figma.currentPage.selection.forEach(node => {
     if (node.type == 'TEXT') {
         node.autoRename = true;
-        //node.name = '';
+        figma.closePlugin(layerCount + " Text layer names reset ✅");
+        return;
+    }
+    else {
+        figma.notify("That's not a Text layer silly!", { timeout: 2000 });
+        figma.closePlugin();
     }
 });
-figma.closePlugin();
--- code.ts
+++ code.ts
@@ -1,7 +1,14 @@
+let layerCount = (figma.currentPage.selection.length)
+
 figma.currentPage.selection.forEach(node => {
   if (node.type == 'TEXT') {
     node.autoRename = true;
+    figma.closePlugin(layerCount + " Text layer names reset ✅");
+    return
+  }
+
+  else {
+    figma.notify("That's not a Text layer silly!", { timeout: 2000 });
+    figma.closePlugin();
   }
 })
-
-figma.closePlugin()
