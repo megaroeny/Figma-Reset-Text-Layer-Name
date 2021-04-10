52e630b63e96e6ea193d327a7b216876700ac8eb
Yury Zeliankouski <yuri@zend.pro>
2021-04-10 18:47:07 +0300

    Redesigned plugin code
    Added an edge case (notification if nothing is selected and close the plugin), improved notifications about non-text layers and about successful reset of text layer names.

--- code.js
+++ code.js
@@ -1,12 +1,22 @@
-let layerCount = (figma.currentPage.selection.length);
+let successCount = 0,
+	layerCount = figma.currentPage.selection.length;
+
+if (layerCount === 0) {
+	figma.closePlugin("Select at least one Text Layer")
+}
+
 figma.currentPage.selection.forEach(node => {
-    if (node.type == 'TEXT') {
-        node.name = "";
-        figma.closePlugin(layerCount + " Text layer names reset ✅");
-        return;
-    }
-    else {
-        figma.notify("That's not a Text layer silly!", { timeout: 2000 });
-        figma.closePlugin();
-    }
-});
+	if (node.type === "TEXT") {
+		node.name = "";
+		successCount++
+	}
+	else {
+		figma.notify(node.name + " not a Text Layer", { timeout: 2000 })
+	}
+})
+
+if (layerCount >= 1) {
+	figma.notify(successCount + " Text Layer names reset ✅", { timeout: 2000 })
+}
+
+figma.closePlugin();
